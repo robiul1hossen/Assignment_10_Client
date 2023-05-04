99a7e09bb51efdec6df66a1fe0c87e0a4fb7414a
robiul1hossen <robim6225@gmail.com>
2023-05-04 14:35:18 +0600

    blog section updated with faq

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -6,10 +6,44 @@ const ref = React.createRef();
 
 const blogs = () => {
   return (
-    <Container>
+    <Container className="w-50">
       <div className="App">
         <div ref={ref}>
-          <h2>This is blog component</h2>
+          <div>
+            <h4>1. The differences between uncontrolled and controlled components.</h4>
+            <p>
+              uncontrolled components are managed by external libraries or frameworks and cannot be fully
+              customized, while controlled components are developed in-house and can be fully customized and
+              controlled by the developer.
+            </p>
+          </div>
+          <div>
+            <h4>2. How to validate React props using PropTypes</h4>
+            <p>
+              React provides a utility library called PropTypes that allows you to define the type and shape
+              of props that a component should receive. This can help you catch errors early on by checking if
+              the props passed to a component match the expected type and structure.
+            </p>
+          </div>
+          <div>
+            <h4>3. The difference between nodejs and express js.</h4>
+            <p>
+              Node.js is a server-side JavaScript runtime environment that provides a platform for building
+              scalable, networked applications, while Express.js is a web application framework built on top
+              of Node.js that provides a set of high-level features and utilities for building web
+              applications and APIs.
+            </p>
+          </div>
+          <div>
+            <h4>4.What is a custom hook, and why will you create a custom hook?</h4>
+            <p>
+              A custom hook is a function in React that allows you to reuse stateful logic across multiple
+              components. Custom hooks allow you to encapsulate complex logic and reuse it across different
+              components without having to repeat the code. Custom hooks are created using the use prefix and
+              follow the same rules as React's built-in hooks. They can call other hooks or use other
+              functions, and they can also return data, just like regular functions.
+            </p>
+          </div>
         </div>
         <Pdf targetRef={ref} filename="code-example.pdf">
           {({ toPdf }) => (

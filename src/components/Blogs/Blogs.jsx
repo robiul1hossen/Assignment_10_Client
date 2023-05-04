2bc306095e6da64687fc0e6a89614c5732d92262
robiul1hossen <robim6225@gmail.com>
2023-05-04 14:04:16 +0600

    fun pdf added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const blogs = () => {
   return (
     <Container>
-      <h2>This is blog container</h2>
+      <div className="App">
+        <div ref={ref}>
+          <h2>This is blog component</h2>
+        </div>
+        <Pdf targetRef={ref} filename="code-example.pdf">
+          {({ toPdf }) => (
+            <Button className="my-4" onClick={toPdf}>
+              Download Pdf
+            </Button>
+          )}
+        </Pdf>
+      </div>
     </Container>
   );
 };
